f3c4c9c2065f24d590e0ccafd3b09c0c7c1d9dc9
SartoriGilson <gilsonsartori80@gmail.com>
2024-10-24 08:49:22 -0300

    Versão 13

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -134,5 +134,9 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4
+    'PAGE_SIZE': 4,
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
