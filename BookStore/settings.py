8e9135808658d0e5dfe550495339ade20ba3c044
SartoriGilson <gilsonsartori80@gmail.com>
2024-10-28 18:09:27 -0300

    Versão 15

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'BookStore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get("SQL_ENGINE","django.db.backends.sqlite3"),
-        'NAME': os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", BASE_DIR / 'db.sqlite3'),
         'USER': os.environ.get("SQL_USER", "user"),
         'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
         'HOST': os.environ.get("SQL_HOST", "localhost"),
@@ -150,8 +150,8 @@ INTERNAL_IPS = [
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG, default=0"))
+DEBUG = os.environ.get("DEBUG") == '1'
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space betwen each.
 # For example : 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS','').split(" ")
--- Dockerfile
+++ Dockerfile
@@ -41,6 +41,11 @@ RUN apt-get update \
 # install poetry - respects $POETRY_VERSION & $POETRY_HOME
 RUN curl -sSL https://install.python-poetry.org | python3
 
+# install postgress dependencies
+RUN apt-get update \
+    && apt-get -y install libpq-dev gcc \
+    && pip install psycopg2
+
 # copy project requirement files here to ensure they will be cached.
 WORKDIR $PYSETUP_PATH
 COPY poetry.lock pyproject.toml ./
