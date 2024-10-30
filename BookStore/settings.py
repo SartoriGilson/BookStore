39dada833637ab5369d67ffb7189298c67c9a999
SartoriGilson <gilsonsartori80@gmail.com>
2024-10-30 13:06:47 -0300

    deploy

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -154,4 +154,5 @@ DEBUG = os.environ.get("DEBUG") == '1'
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space betwen each.
 # For example : 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS','').split(" ")
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://bookstore-v1-4024d83e233f.herokuapp.com']
